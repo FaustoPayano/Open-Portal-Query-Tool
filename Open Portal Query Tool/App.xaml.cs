c5db99434ed1c2d5b30f7c2344f567c6187f527b
Fausto Payano <fap2117@columbia.edu>
2016-03-14 17:23:50 -0400

    Initial Commit
    V2 using Material Design, will also implement API 2.1 features.

---
+++ Open Portal Query Tool/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace Open_Portal_Query_Tool {
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application {
+    }
+}
